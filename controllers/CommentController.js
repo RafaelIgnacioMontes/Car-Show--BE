4cf845465fb725934a96f2e7e7f076cea1e90e48
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-03-03 14:33:54 -0700

    Comment routes

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -9,6 +9,44 @@ const GetAllComments = async (req, res) => {
   }
 }
 
+const CreateComment = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id)
+    let carId = parseInt(req.params.car_id)
+    let commentBody = {
+      userId,
+      carId,
+      ...req.body
+    }
+    let comment = await Comment.create(commentBody)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    let updateComment = await Comment.update(req.body, {
+      where: { id: commentId },
+      returning: true
+    })
+    res.send(updateComment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteComment = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
-  GetAllComments
+  GetAllComments,
+  UpdateComment,
+  CreateComment
 }
