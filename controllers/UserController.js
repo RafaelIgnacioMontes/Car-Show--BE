3f48f3980ddaf9fb4ab09ce7dc5aa56481650c7f
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-03-03 13:25:23 -0700

    added controllers and routes for USER

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,1 +1,14 @@
 const { User } = require('../models')
+
+const GetUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({ attribute: userName })
+    res.send(users)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetUsers
+}
