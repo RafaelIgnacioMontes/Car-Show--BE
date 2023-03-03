aaf545a0c374a56a5a286f6a6b4f3c773cfe392f
Clifton Lucas <cliftonlucas89@gmail.com>
2023-03-03 12:03:17 -0600

    update as

--- migrations/20230303170307-create-car.js
+++ migrations/20230303170307-create-car.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -35,9 +35,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Cars');
+    await queryInterface.dropTable('Cars')
   }
-};
+}
