ea7bcea907f6f53681179129140259b8f870efd6
Clifton Lucas <cliftonlucas89@gmail.com>
2023-03-09 08:21:22 -0600

    merge

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -5,21 +5,21 @@ const middleware = require('../middleware')
 router.get('/all', controller.GetAllComments)
 router.post(
   '/newcomment/:car_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.CreateComment
 )
 router.put(
   '/update/:comment_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.UpdateComment
 )
 router.get('all/:car_id', controller.GetAllCommentsForCar)
 router.delete(
   '/delete/:comment_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.DeleteComment
 )
 
